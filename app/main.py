584d181e5455a02b245dca97d3422e9abdaa9bb3
ErikRm1962 <erikrm9086@gmail.com>
2026-07-03 23:49:20 -0500

    Reestructuracion: main.py usa enrutadores, listas centralizadas en listas.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .enrutadores.clientes import rutas_clientes
+from .enrutadores.facturas import rutas_facturas
+from .enrutadores.transacciones import rutas_transacciones
+
+app = FastAPI(title="API Facturación - Clientes")
+
+# Incluir rutas
+app.include_router(rutas_clientes, tags=["Clientes"])
+app.include_router(rutas_facturas, tags=["Facturas"])
+app.include_router(rutas_transacciones, tags=["Transacciones"])
